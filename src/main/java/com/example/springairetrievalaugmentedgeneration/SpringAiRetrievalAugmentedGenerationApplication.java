9c14f4889432d2a00784467ce51d014613af86b6
Dominik Jałowiecki <dominik.jalowiecki1@gmail.com>
2025-04-06 10:50:37 +0200

    Project init

---
+++ src/main/java/com/example/springairetrievalaugmentedgeneration/SpringAiRetrievalAugmentedGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springairetrievalaugmentedgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRetrievalAugmentedGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiRetrievalAugmentedGenerationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springairetrievalaugmentedgeneration/SpringAiRetrievalAugmentedGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springairetrievalaugmentedgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRetrievalAugmentedGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
